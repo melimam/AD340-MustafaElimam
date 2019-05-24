2addc9c0693089ced5bb2b0a47596bc51e71bcc8
Mustafa Elimam <mjkam88@gmail.com>
2019-05-24 10:38:53 -0700

    changes for button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void buttonClick (View view){
+        Button b = (Button) view;
+        String label = b.getText().toString().toLowerCase();
+    Log.d("BUTTON", "buttonClick");
+        switch (label){
+            case "movies":
+                Intent E = new Intent(MainActivity.this, Movielist.class);
+
+                startActivity(E);
+
+        }
+        Toast.makeText(MainActivity.this,label, Toast.LENGTH_LONG). show();
+    }
 }
+
